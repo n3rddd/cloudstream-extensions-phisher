c4d66809cdbd69836a2cba6e28fa662f7cc4eab2
Phisher98 <mandiktest2@gmail.com>
2026-02-25 17:54:51 +0530

    Minor Fix

--- Animesalt/build.gradle.kts
+++ Animesalt/build.gradle.kts
@@ -22,7 +22,7 @@ cloudstream {
         "Cartoon"
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=animesalt.cc&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=animesalt.top&sz=%size%"
 
     isCrossPlatform = true
 }
